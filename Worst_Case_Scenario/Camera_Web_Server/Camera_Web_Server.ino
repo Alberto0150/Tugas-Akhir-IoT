5c356e6236ddaf0692d9996fe8f86cc6ded9a7fa
Alberto0150 <albertosanjaya2@gmail.com>
2022-02-09 20:29:17 +0700

    Update Camera_Web_Server.ino

--- Worst_Case_Scenario/Camera_Web_Server/Camera_Web_Server.ino
+++ Worst_Case_Scenario/Camera_Web_Server/Camera_Web_Server.ino
@@ -7,34 +7,17 @@
 //            Partial images will be transmitted if image exceeds buffer size
 //
 
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
 
 #include "camera_pins.h"
 
 const char* ssid = "Alberto";
 const char* password = "albertoooo";
 
-//// Set Static IP address
-//IPAddress local_IP(192, 168, 149, 156);
-//// Set Gateway IP address
-//IPAddress gateway(192, 168, 149, 68);
-////Setup Subnet
-//IPAddress subnet(255, 255, 255, 0);
-//IPAddress primaryDNS(8, 8, 8, 8); 
-//IPAddress secondaryDNS(8, 8, 4, 4);
-
-
 void startCameraServer();
 
 #define LAMP 4
+#define RELAY_COM_1 1
 
 void setup() {
   Serial.begin(115200);
@@ -74,12 +57,7 @@ void setup() {
     config.jpeg_quality = 12;
     config.fb_count = 1;
   }
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
+  
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -88,25 +66,11 @@ void setup() {
   }
 
   sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
+  
   // drop down frame size for higher initial frame rate
   s->set_framesize(s, FRAMESIZE_QVGA);
 
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
   WiFi.begin(ssid, password);
-
-//  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-//    Serial.println("STA Failed to configure");
-//  }
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -119,13 +83,19 @@ void setup() {
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+  Serial.println("/stream' to connect");
 
   pinMode(LAMP, OUTPUT);
+  pinMode(RELAY_COM_1, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LAMP, HIGH);
+  if(detected == false){
+      digitalWrite(pinDigitalRelay1, LOW); //Normally open pin
+  }else{
+    digitalWrite(pinDigitalRelay1, HIGH); //Normally open pin
+  }
 //  delay(10000);
 }
