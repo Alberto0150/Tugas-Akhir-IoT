7a0c12492af9729cfc2defd0deefafb7e6a15a96
Alberto0150 <albertosanjaya2@gmail.com>
2022-05-31 11:55:11 +0700

    Fixing actual os.chdir() problem
    need to create fill -result.txt first

--- Core_Program/main.py
+++ Core_Program/main.py
@@ -10,8 +10,8 @@ thread_list = {}
 flag = 0
 
 time_to_loop_per_sec = 5
-IP_ESP_Cam_Array = ["192.168.132.172",
-                    "192.168.132.156",
+IP_ESP_Cam_Array = ["192.168.158.172",
+                    "192.168.158.156",
                     ] # Set IP
 total_ESP = len(IP_ESP_Cam_Array) + 1
 
@@ -32,22 +32,22 @@ def thread_task(current_IP):
     time.sleep(time_to_loop_per_sec)
 
     # Execute Capture Image
-    try:
-        os.chdir(path=saving_image_path)
-    except:
-        pass
+    # try:
+    #     os.chdir(path=saving_image_path)
+    # except:
+    #     pass
     # current_location = os.getcwd()
     # if saving_image_path not in current_location:
         # os.chdir(path=saving_image_path)
         # print('Change to image location')
-    image_capture.capture_mode(current_IP,'1', exec_chrome_driver_path)
+    image_capture.capture_mode(current_IP,'1', exec_chrome_driver_path,saving_image_path)
 
     # Current exec location : @saving_image_path → check before running
     # Change back location
     current_location = os.getcwd()
-    # if current_location not in default_location:
-    os.chdir(path=default_location)
-    print('Change to default location')
+    if current_location not in default_location:
+        os.chdir(path=default_location)
+        print('Change to default location')
         
 
     # Execute Yolo Program
@@ -57,7 +57,8 @@ def thread_task(current_IP):
     stdoutdata, stderrdata = running_program.communicate()
 
     # Check if "person"
-    result_path = saving_image_path + "/" + current_IP + "-result.txt"
+    #result_path = saving_image_path + "/" + current_IP + "-result.txt"
+    result_path = saving_image_path + current_IP + "-result.txt"
     result_file = open(result_path, "r")
     if  'person' in result_file.read():
         get_request.sending_get_request(current_IP,0)
@@ -88,6 +89,10 @@ def create_thread():
         
 if __name__ == '__main__':
     
+    for current_IP in IP_ESP_Cam_Array:
+        saving_txt_detection_location = 'D:\\Coding-Tugas-Akhir\\Main-Image-Captured\\'+current_IP+'-result.txt'
+        result_file = open(saving_txt_detection_location,"w")
+        result_file.close()
     # while True:
     for current_IP in IP_ESP_Cam_Array:
         # Creating thread
